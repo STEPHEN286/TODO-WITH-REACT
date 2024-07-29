53b4d37e29675d8a9b237addee599243a717b4ef
STEPHEN286 <kwofie286@gmail.com>
2024-07-29 20:39:44 +0000

    changing the the state of selectedProject to currentProject

--- src/App.js
+++ src/App.js
@@ -2,40 +2,58 @@ import NoProject from "./assets/components/NoProject";
 import Sidebar from "./assets/components/Sidebar";
 import React, { useState } from "react";
 import NewProject from "./assets/components/NewProject";
-import { getTimestamp } from "./assets/utils/getTimestamp";
 import AddTask from "./assets/components/AddTask";
 import { v4 as UUIDV4 } from "uuid";
 
 function App() {
-  const [isClicked, setIsClicked] = useState(false);
+  const [isNewProjectClicked, setisNewProjectClicked] = useState(false);
   const [projects, setProjects] = useState([]);
-  const [selectedProject, setSelectedProject] = useState(null);
+  const [currentProject, setCurrentProject] = useState(null);
 
   const handleAddProject = () => {
-    setIsClicked(true);
+    setisNewProjectClicked(true);
   };
 
+  const handleDeleteProject = (projectToDelete) => {
+    console.log("Project to delete:", projectToDelete);
+    console.log("Projects before deletion:", projects);
+    const remainingProjects = projects.filter(
+      (project) => project.id !== projectToDelete.id
+    );
 
-  const handleSelectedProject = (projectId) => {
-    setSelectedProject(projectId);
-    setIsClicked(false);
+    setProjects(remainingProjects);
+
+    console.log("Remaining projects:", remainingProjects);
+
+    if (currentProject && currentProject.id === projectToDelete.id) {
+      setCurrentProject(null);
+    }
   };
 
-  const handleCancel = () => setIsClicked(false)
-  
+  const handleSelectedProject = (project) => {
+    setCurrentProject(project);
+    setisNewProjectClicked(false);
+  };
+
+  const handleCancel = () => setisNewProjectClicked(false);
+
   function handleSaveProject(projectData) {
     const newProject = { ...projectData, id: UUIDV4() };
     setProjects((prevProjects) => [...prevProjects, newProject]);
-    setIsClicked(false);
+    setisNewProjectClicked(false);
   }
 
   let displayContent;
 
-  if (isClicked) {
-    displayContent = <NewProject onCancel={handleCancel} onAdd={handleSaveProject} />;
-  } else if (selectedProject) {
-    displayContent = <AddTask projects={projects} />;
-  } else if (!isClicked) {
+  if (isNewProjectClicked) {
+    displayContent = (
+      <NewProject onCancel={handleCancel} onAdd={handleSaveProject} />
+    );
+  } else if (currentProject) {
+    displayContent = (
+      <AddTask project={currentProject} onDeleteProject={handleDeleteProject} />
+    );
+  } else if (isNewProjectClicked === false) {
     displayContent = <NoProject onClick={handleAddProject} />;
   }
 
