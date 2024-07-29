5513cb96b1f11ec7388d2c2053f5fb1e7ffa807f
STEPHEN286 <kwofie286@gmail.com>
2024-07-29 20:41:35 +0000

    delete project  functionality

--- src/assets/components/AddTask.js
+++ src/assets/components/AddTask.js
@@ -1,21 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { v4 as Id } from "uuid";
 
-function AddTask({ projects }) {
-  const { id, tittleRef } = projects;
-  // const findItem = projects.find((item) => item.id === id);
+function AddTask({ project, onDeleteProject }) {
+  const [tasks, setTasks] = useState([]);
+  const taskInputRef  
+  function handleAddTask() {
 
-  // console.log("findId", findItem);
-  projects.map((project) => {
-    
-  })
+  }
   return (
     <div className="w-4/5 p-9">
       <div className="border-b py-2">
-        <div flex className="flex  justify-between items-center">
-          <h1 className="font-extrabold text-3xl">Title</h1>
-          <button>delete</button>
+        <div className="flex  justify-between items-center">
+          <h1 className="font-extrabold text-3xl">{project.titleRef}</h1>
+          <button onClick={() => onDeleteProject(project)}>delete</button>
         </div>
-        <p className="text-gray-500">Date</p>
+        <p className="text-gray-500">{project.dateRef}</p>
 
         <p>Description</p>
       </div>
@@ -26,9 +25,14 @@ function AddTask({ projects }) {
           <input
             type="text"
             id="username"
-            class="p-2  bg-gray-200 border-b-0 rounded outline-none  focus:border-b-2 border-blue-900 "
+            className="p-2  bg-gray-200 border-b-0 rounded outline-none  focus:border-b-2 border-blue-900 "
           />
-          <button>add task</button>
+          <button
+            className="bg-blue-950 text-white p-2 rounded"
+            onClick={handleAddTask}
+          >
+            add task
+          </button>
         </div>
         {/* display task */}
         <div className="mt-5  bg-gray-200 p-3 rounded">
--- src/assets/components/Sidebar.js
+++ src/assets/components/Sidebar.js
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import AddTask from "./AddTask";
-
 function Sidebar({ onClick, projects, onProjectSelect }) {
   return (
     <div className=" w-1/3   bg-blue-950 py-16 px-8  md:w-72">
@@ -16,11 +13,12 @@ function Sidebar({ onClick, projects, onProjectSelect }) {
 
       <div className="mt-5">
         {projects.map((project) => {
-          // console.log("sidebar/projects/project", project);
-          console.log(project.id);
           return (
             <p
-              onClick={() => onProjectSelect(project.id)}
+              onClick={() => {
+                onProjectSelect(project);
+                console.log(project.id);
+              }}
               key={project.id}
               className="text-gray-500 font-medium py-2 px-1 hover:bg-blue-900 hover:text-white"
             >
